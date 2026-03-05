65e1cdf0bae2f10a9c5f9c639a0cb9b082e2f72e
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-03-05 09:55:56 +0900

    fix: use db:migrate in e2e global setup

--- e2e/global-setup.ts
+++ e2e/global-setup.ts
@@ -1,5 +1,5 @@
 import { execAsync } from "./helper";
 
 export default async function globalSetup() {
-  await execAsync("pnpm db:reset --persist-to .wrangler/state/e2e");
+  await execAsync("pnpm db:migrate --persist-to .wrangler/state/e2e");
 }
