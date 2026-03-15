f8ff05e28d41a28aa4ef665b74aef9a7fc779914
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-03-15 11:46:47 +0900

    chore: comment

--- src/query.d.ts
+++ src/query.d.ts
@@ -1,5 +1,6 @@
 import "@tanstack/react-query";
 
+// https://tanstack.com/query/latest/docs/framework/react/typescript#registering-global-meta
 declare module "@tanstack/react-query" {
   interface Register {
     mutationMeta: {
